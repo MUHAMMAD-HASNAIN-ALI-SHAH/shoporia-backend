7e24a0fe53c9c249ede3a7a1c4689654bd24355a
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-08-19 20:12:46 +0500

    ready for development

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(
   })
 );
 
+app.set("trust proxy", 1);
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -24,10 +26,10 @@ app.use(
       collectionName: "sessions",
     }),
     cookie: {
-      secure: false,
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "lax",
     },
   })
 );
